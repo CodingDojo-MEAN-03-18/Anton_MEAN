11b4adff0e335986aa27d6258e05f87fbcecbbf3
Anton Silivonchyk <slntn@yahoo.com>
2018-03-18 22:29:17 -0400

    car - completed;

---
+++ Node/cars_and_cats/app.js
@@ -0,0 +1,76 @@
+var http = require('http');
+var fs = require('fs');
+var port = 7077;
+
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/cars') {
+        fs.readFile('views/cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }else if(request.url === '/cats'){
+        fs.readFile('views/cats.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }else if(request.url === '/cars/new'){
+        fs.readFile('views/new.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }else if(request.url === '/stylesheets/style.css'){
+        fs.readFile('./stylesheets/style.css', 'utf8', function(errors, contents){
+         response.writeHead(200, {'Content-type': 'text/css'});
+         response.write(contents);
+         response.end();
+        })
+    } else if(request.url === '/images/1.jpg'){
+        fs.readFile('./images/1.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }else if(request.url === '/images/2.jpg'){
+        fs.readFile('./images/2.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }else if(request.url === '/images/3.jpg'){
+        fs.readFile('./images/3.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }else if(request.url === '/images/c1.jpg'){
+        fs.readFile('./images/c1.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }else if(request.url === '/images/c2.jpg'){
+        fs.readFile('./images/c2.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }else if(request.url === '/images/c3.jpg'){
+        fs.readFile('./images/c3.jpg', function(errors, contents){
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }else {// request didn't match anything:
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+
+server.listen(port);
+console.log("Running in localhost at port " + port);
